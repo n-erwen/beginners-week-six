c006ff7adad197bdaa3efcc9f9a99cbd0c2930d2
n-erwen <54708198+n-erwen@users.noreply.github.com>
2019-11-07 15:52:22 +0000

    Initial commit

---
+++ js/background.js
@@ -0,0 +1,34 @@
+// writes to the log 'hello'
+console.log('hello!')
+
+// three different variables for the different images
+var backgrounds = ['mike-yukhtenko', 'gabriele-garanzelli', 'fabian-oelkers']
+var names = ['Mike Yukhtenko','Gabriele Garanzelli','Fabian Oelkers']
+
+// Only fires logic after the page has fully loaded
+$( document ).ready(function(){
+	
+  // when the button is clicked it triggers a function
+  $('.jqbutton').on('click', function () {
+
+    // 1. log to the console that you have clicked the button
+
+    // this selects a randon background from above and stores it as the variable 'randomBackground'
+	var i = Math.floor(Math.random() * backgrounds.length)
+    var randomBackground = backgrounds[i];
+	var artist = names[i]
+    // this creates a string that css will understand to be a background url
+    var imageUrl = "url('./images/" + randomBackground + ".jpg')"
+    
+    
+    // 2. using jquery, change the image url of the body background to the new one saved above
+	
+	$('body').css('background',imageUrl)
+    // EXTENSION TASK!
+    // 3. Change the text of the 'span' to be that of the photographer
+    //   (hint, you should re-use randomBackgroun)
+    // Double points if you replace '-' between their names with a space
+	$('.photographer').text(artist)
+    
+  }) 
+}) // here endeth the function
